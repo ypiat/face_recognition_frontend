aca6aea28ffdbbd4e7a3c862b8a788318f900200
ypiat <108991163+ypiat@users.noreply.github.com>
2023-01-24 12:48:47 +0100

    Update ImageLinkForm.js

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit}) => {
 	return (
 		<div>
 			<p className='f3'>
-				{'This Magic Magnifier will detect faces in your pictures. Give it a try.'}
+				{'This Magic Magnifier will detect face in your pictures. Give it a try.'}
 			</p>
 			<div className='center'>
 			  <div className='form center pa4 br3 shadow-5'>
